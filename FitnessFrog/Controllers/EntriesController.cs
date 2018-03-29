7d2c1d991cf4900b2df135a980b0041356b65df9
Vic Johnson <vjohnson@imagetrend.com>
2018-03-29 10:23:03 -0500

    Added some temp data to reflect the CRUD changes after they have been completed so that users know that the actions were successfully performed.

--- FitnessFrog/Controllers/EntriesController.cs
+++ FitnessFrog/Controllers/EntriesController.cs
@@ -57,6 +57,8 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.AddEntry(entry);
 
+                TempData["Message"] = "Your entry was successfully added";
+
                 return RedirectToAction("Index");
             }
 
@@ -91,6 +93,8 @@ namespace Treehouse.FitnessFrog.Controllers
             {
                 _entriesRepository.UpdateEntry(entry);
 
+                TempData["Message"] = "Your Entry was successfully updated!";
+
                 return RedirectToAction("Index");
             }
 
@@ -120,6 +124,8 @@ namespace Treehouse.FitnessFrog.Controllers
         public ActionResult Delete(int id)
         {
             _entriesRepository.DeleteEntry(id);
+
+            TempData["Message"] = "Your entry was successfully deleted!";
 
             return RedirectToAction("Index");
         }
